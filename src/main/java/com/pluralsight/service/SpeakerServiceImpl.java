bb66370d83a6cd423d93f3b978d191630380bb00
Farrah Carter <carterfarraha@gmail.com>
2020-04-20 14:17:36 -0400

    Added autowiring.

--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -3,14 +3,24 @@ package com.pluralsight.service;
 import com.pluralsight.model.Speaker;
 import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.repository.SpeakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service("speakerService")
 public class SpeakerServiceImpl implements SpeakerService {
 
     private SpeakerRepository repository;
 
+    public SpeakerServiceImpl(){
+        System.out.println("SpeakServiceImpl no args constructor");
+    }
+
+    @Autowired
     public SpeakerServiceImpl(SpeakerRepository speakerRepository){
+        System.out.println("SpeakServiceImpl repository constructor");
         repository = speakerRepository;
     }
 
@@ -19,6 +29,7 @@ public class SpeakerServiceImpl implements SpeakerService {
     }
 
     public void setRepository(SpeakerRepository repository) {
+        System.out.println("SpeakServiceImpl setter");
         this.repository = repository;
     }
 }
